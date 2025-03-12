ba0a76dce43b8fe1909dcc2cc7db4db1a73bd3f2
ykpark0911 <ykpark20060911@gmail.com>
2025-03-12 14:11:00 +0900

    dd

--- inchTocm.py
+++ inchTocm.py
@@ -1,2 +1,3 @@
-print("인치 ---> 센티미터 변환기")
-print(str(float(input("인치 값 입력: "))/39.37) + "cm")
+inch= int(input("인치를 입력해주세요"))
+cm= inch * 2.542
+print(cm)
---
+++ output.py
@@ -0,0 +1,26 @@
+# 하나만 출력합니다.
+print("#하나만 출력합니다.")
+print("Hello Python Programming...!")
+print()
+
+# 여러 개를 출력합니다
+print("# 여러 개를 출력합니다.")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의", "이름은", "박연규입니다!")
+print()
+
+print("이름\t\나이\t지역")
+print("박연규\t25\t강서구")
+print("\\ \\ \\ \\")
+print("안녕하세요" * 3)
+
+print("# 문자 선택 연산자")
+print("안녕하세요"[0])
+print("안녕하세요"[1])
+print("안녕하세요"[2])
+print("안녕하세요"[3])
+print("안녕하세요"[4])
+
+print(type(len("안녕하세요"))) # len은 int로 나옴, type
+
+string = input("입력> ")
