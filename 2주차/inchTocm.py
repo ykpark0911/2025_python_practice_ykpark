87199c091cb2162acc62ff76c99bf1896b332b1f
ykpark0911 <ykpark20060911@gmail.com>
2025-03-18 10:52:59 +0900

    a

--- 2주차/inchTocm.py
+++ 2주차/inchTocm.py
@@ -1,3 +1,3 @@
 inch= int(input("인치를 입력해주세요 "))
 cm= inch * 2.542
-print(cm, "센치미터입니다")
+print(str(cm) + "센치미터입니다")
--- 2주차/kgToPound.py
+++ 2주차/kgToPound.py
@@ -1,3 +1,3 @@
 kg = int(input("kg을 입력해주세요 "))
 ld = kg*2.20462
-print(ld, "파운드입니다")
+print(str(ld) + "파운드입니다")
