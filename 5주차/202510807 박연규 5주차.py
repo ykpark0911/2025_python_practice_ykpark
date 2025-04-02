f5b1ca3a2b5aafa7268754679507cb2ce9813749
박연규 <ykpark20060911@gmail.com>
2025-04-02 10:26:40 +0900

    완성

--- 5주차/202510807 박연규 5주차.py
+++ 5주차/202510807 박연규 5주차.py
@@ -1,7 +1,10 @@
+#각 단의 제목 출력
 for i in range(2, 10):
-    print("#  {}단  # ".format(i), end = "")
+    print("#  {}단  # ".format(i), end = "") #end = "" 는 줄바꿈 하지 않겠다는 것을 의미
 
-for num in range(1, 10): #계수
+#각 (세로)줄을 출력
+for num in range(1, 10): #num: 곱해지는 수
     print()
-    for dan in range(2, 10): #단
-        print("{} {:>2} ".format(f"{dan}X  {num}=", f"{dan*num}"), end = "")
+
+    for dan in range(2, 10): #dan: 단
+        print(f"{dan}X  {num}= {dan*num:>2} ", end = "") # "{단}X  {곱해지는 수}= {결과} " 형식으로 출력 ({결과}는 오른쪽 정렬)
